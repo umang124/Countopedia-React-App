1b8c80629396b3e5f0130ef2ef01725b3bbb6310
Umang Shrestha <umangstha124@gmail.com>
2023-11-23 13:28:41 +0545

    click events

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
 class Counter extends React.Component {
+  handleAttack() {
+    alert("Attack clicked");
+  }
+
+  handleDefend() {
+    alert("Defend Clicked");
+  }
+
   render() {
     return (
       <div className="row text-white">
         <h1>Counter: </h1>
-        <button style={{ width: "200px" }}>+1</button>
-        <button style={{ width: "200px" }}>-1</button>
+        <button onClick={this.handleAttack} style={{ width: "200px" }}>
+          +1
+        </button>
+        <button onClick={this.handleDefend} style={{ width: "200px" }}>
+          -1
+        </button>
       </div>
     );
   }
