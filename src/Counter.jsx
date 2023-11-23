c0b899dd7c186b25163b69f72add83c62e27ec70
Umang Shrestha <umangstha124@gmail.com>
2023-11-23 17:42:08 +0545

    Random play and Reset Button

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,41 +1,100 @@
 import React from "react";
+import attack from "./images/attack.png";
+import defend from "./images/defend.png";
 
 class Counter extends React.Component {
   constructor(props) {
     super(props);
-    this.handleAttack = this.handleAttack.bind(this);
-    this.handleDefend = this.handleDefend.bind(this);
     this.state = {
       count: 0,
     };
   }
 
-  handleAttack() {
+  handleAttack = () => {
     this.setState((previousState) => {
+      let newCount = previousState.count + Math.round(Math.random() * 10);
       return {
-        count: previousState.count + 1,
+        count: newCount,
       };
     });
-  }
+  };
 
-  handleDefend() {
+  handleDefend = () => {
     this.setState((previousState) => {
+      let newCount = previousState.count - Math.round(Math.random() * 10);
       return {
-        count: previousState.count - 1,
+        count: newCount,
       };
     });
-  }
+  };
+
+  handleRandomPlay = () => {
+    let playMode = Math.round(Math.random());
+    if (playMode === 0) {
+      this.handleAttack();
+    } else {
+      this.handleDefend();
+    }
+  };
+
+  handleReset = () => {
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
+  };
 
   render() {
     return (
-      <div className="row text-white">
-        <h1>Counter: {this.state.count}</h1>
-        <button onClick={this.handleAttack} style={{ width: "200px" }}>
-          +1
-        </button>
-        <button onClick={this.handleDefend} style={{ width: "200px" }}>
-          -1
-        </button>
+      <div className="row text-white text-center">
+        <h1>Game Score: {this.state.count}</h1>
+        <p>You win at +10 points and lose at -10 points!</p>
+        <p>Last play: </p>
+        <p>Game Status: </p>
+        <div className="col-6 col-md-6">
+          <img
+            src={attack}
+            style={{
+              width: "50%",
+              cursor: "pointer",
+              border: "1px solid green",
+            }}
+            className="p-4 rounded"
+            onClick={this.handleAttack}
+            alt=""
+          />
+        </div>
+
+        <div className="col-6 col-md-6">
+          <img
+            src={defend}
+            style={{
+              width: "50%",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+            onClick={this.handleDefend}
+            alt=""
+          />
+        </div>
+
+        <div className="col-12 col-md-4 offset-md-4 mb-4">
+          <button
+            className="btn btn-secondary w-100 mt-2"
+            onClick={this.handleRandomPlay}
+          >
+            Random Play
+          </button>
+          <br />
+          <button
+            className="btn btn-warning w-100 mt-2"
+            onClick={this.handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     );
   }
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,9 +2,9 @@ import logo from "./images/react.png";
 
 function MainHeader() {
   return (
-    <div className="pt-3 py-1 pl-2" style={{ borderBottom: "1px solid #777" }}>
+    <div className="py-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
       <img src={logo} alt="" style={{ height: "35px", verticalAlign: "top" }} />
-      <span className="h2 pt-4 text-white-50">React Course - Countopedia</span>
+      <span className="h2 pt-4 text-white-50">Countopedia</span>
     </div>
   );
 }
